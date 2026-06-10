a15801bbec6b4d2ffa8bfcf541a3f01ee2a341ab
Andris Veips <av00717@rvt.lv>
2026-06-10 15:07:02 +0300

    fix trust proxies headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: '*', headers: 0x1f);
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
