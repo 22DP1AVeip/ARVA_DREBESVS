4245978f60feeb03ad9f1fc394500bed670ff188
Andris Veips <av00717@rvt.lv>
2026-06-10 14:05:39 +0300

    use migrate fresh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-php artisan migrate --force --ignore-errors 2>/dev/null || true
-php artisan migrate --force
+php artisan migrate:fresh --force
 php artisan db:seed --class=ProductSeeder --force
 php artisan serve --host=0.0.0.0 --port=3000
