96a454d9060a776980de7059dddaea334bd4a664
Andris Veips <av00717@rvt.lv>
2026-06-10 15:02:03 +0300

    fix session issues

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 php artisan migrate:fresh --force
 php artisan db:seed --class=ProductSeeder --force
+php artisan key:generate --force
 php artisan config:clear
+php artisan cache:clear
 php artisan serve --host=0.0.0.0 --port=3000
