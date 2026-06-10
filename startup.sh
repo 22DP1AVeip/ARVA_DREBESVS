50e2649eff65ff87aa5509fac872db2a07b33c03
Andris Veips <av00717@rvt.lv>
2026-06-10 16:50:52 +0300

    jauns starup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-php artisan migrate:fresh --force
+php artisan migrate --force
 php artisan db:seed --class=ProductSeeder --force
-php artisan key:generate --force
 php artisan config:clear
 php artisan cache:clear
 php -S 0.0.0.0:3000 -t public
