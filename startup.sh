70dbdc8134b642b6f2b7656cfca108360d047b80
Andris Veips <av00717@rvt.lv>
2026-06-10 15:14:36 +0300

    switch to php built-in server

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ php artisan db:seed --class=ProductSeeder --force
 php artisan key:generate --force
 php artisan config:clear
 php artisan cache:clear
-php artisan serve --host=0.0.0.0 --port=3000
+php -S 0.0.0.0:3000 -t public
